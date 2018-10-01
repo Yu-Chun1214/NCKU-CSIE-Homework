3772d3f45d250847dc73c387eb4089d3642323ec
Yu-Chun1214 <yuchunlin0075@gmail.com>
2018-10-01 13:58:18 +0800

    commit for test the other metho

--- Data-Structure/hw1/hw1.cpp
+++ Data-Structure/hw1/hw1.cpp
@@ -1,16 +1,17 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-int fibonacci1(int num){
+int fibonacci_recursive(int num){
     if(num == 0)
         return 0;
     else if(num == 1)
         return 1;
     else
-        return fibonacci1(num - 1) + fibonacci1(num -2);
+        return fibonacci_recursive(num - 1) + fibonacci_recursive(num -2);
 }
 
-int fibonacci2(int num){
+int fibonacci_iterative(int num){
     int an_2 = 0,an_1 = 1;
     int ans,temp;
     if (num == 0)
@@ -28,27 +29,17 @@ int fibonacci2(int num){
 }
 
 int main(int argc, char **argvs){
-    int num = atoi(argvs[1]);
-    int ans1,ans2;
-    for(int i = 0;i<=num;++i){
-        ans1 = fibonacci1(i);
-        ans2 = fibonacci2(i);
-        if(ans1 != ans2){
-            printf("f1(%d) = %d\n",i,ans1);
-            printf("f2(%d) = %d\n",i,ans2);
-            system("pause");
-            exit(0);
-        }
-        else{
-            printf("%d %d\n",ans1,ans2);
-        }
+    int num_test_case;
+    int test_case;
+    vector<int> cases;
+    cin>>num_test_case;
+    for(int i = 0;i<num_test_case;i++){
+        cin>>test_case;
+        cases.push_back(test_case);
     }
-    cout<<"All Right"<<endl;
-    return(0);
+    printf("\n");
+    for(vector<int>::iterator it = cases.begin();it!=cases.end();it++){
+        cout<<fibonacci_recursive(*it)<<" "<<fibonacci_iterative(*it)<<endl;
+    }
+    return 0;
 }
-
-// int main(){
-//     for(int i = 0;i<10;i++)
-//         cout<<fibonacci2(i)<<endl;
-//     return 0;
-// }
