b8eca7b51f9db8183bdbf2491672a68605eb282f
Yu-Chun1214 <yuchunlin0075@gmail.com>
2018-10-12 00:06:15 +0800

    commit

--- CS-Code-Design/hw2/hw2.c
+++ CS-Code-Design/hw2/hw2.c
@@ -1,72 +1,72 @@
-#include<stdio.h>
-#include<stdlib.h>
+// #include<stdio.h>
+// #include<stdlib.h>
 
-int SetData(int data[],const char *choice,int P){
-    int i;
-    int scf;
-    printf("%s",choice);
-    for(i = 0; i < P; i++){
-        !scanf("%d",&data[i])?scf=1:0;
-    }
-    while ((getchar()) != '\n');// clear the buffer
-    return 1;
-}
+// int SetData(int data[],const char *choice,int P){
+//     int i;
+//     int scf;
+//     printf("%s",choice);
+//     for(i = 0; i < P; i++){
+//         !scanf("%d",&data[i])?scf=1:0;
+//     }
+//     while ((getchar()) != '\n');// clear the buffer
+//     return 1;
+// }
 
-int Check(int data[],int H,int P){
-    int i = 0;
-    for(i = 0;i < P; i++){
-        if(data[i] < 1 || data[i] > H)
-            return 0;
-    }
-    return 1;
-}
+// int Check(int data[],int H,int P){
+//     int i = 0;
+//     for(i = 0;i < P; i++){
+//         if(data[i] < 1 || data[i] > H)
+//             return 0;
+//     }
+//     return 1;
+// }
 
-void Reset(int data[],int size){
-    for(int i = 0;i < size; i++){
-        data[i] = 0;
-    }
-}
+// void Reset(int data[],int size){
+//     for(int i = 0;i < size; i++){
+//         data[i] = 0;
+//     }
+// }
 
-void HX(int ans[],int P){
-    int ans_match[P];
-    int guess[P];
-    int guess_match[P];
-    int h,x;
+// void HX(int ans[],int P){
+//     int ans_match[P];
+//     int guess[P];
+//     int guess_match[P];
+//     int h,x;
 
-    do{
-        h = 0;
-        x = 0;
-        Reset(ans_match,P);
-        Reset(guess_match,P);
-        SetData(guess,"guess:",P);
-        for(int i = 0;i < P; i++){
-            if(guess[i] == ans[i]){
-                ans_match[i]= guess_match[i] = 1;
-                h++;
-            }
-        }
+//     do{
+//         h = 0;
+//         x = 0;
+//         Reset(ans_match,P);
+//         Reset(guess_match,P);
+//         SetData(guess,"guess:",P);
+//         for(int i = 0;i < P; i++){
+//             if(guess[i] == ans[i]){
+//                 ans_match[i]= guess_match[i] = 1;
+//                 h++;
+//             }
+//         }
 
-        for(int i = 0; i < P; i++){
-            if(guess_match[i] != 1){
-                for(int j = 0; j < P; j++){
-                    if(ans[j] == guess[i]&&ans_match[j] != 1)
-                        x++;
-                }
-            }
-        }
-        printf("%dH%dX\n",h,x);
-    }while(h!=P);
-}
+//         for(int i = 0; i < P; i++){
+//             if(guess_match[i] != 1){
+//                 for(int j = 0; j < P; j++){
+//                     if(ans[j] == guess[i]&&ans_match[j] != 1)
+//                         x++;
+//                 }
+//             }
+//         }
+//         printf("%dH%dX\n",h,x);
+//     }while(h!=P);
+// }
 
-int main(int argc, char const *argv[])
-{
-    int N = atoi(argv[1]);
-    int P = atoi(argv[2]);
-    printf("You set N=%d, P=%d\n",N,P);
+// int main(int argc, char const *argv[])
+// {
+//     int N = atoi(argv[1]);
+//     int P = atoi(argv[2]);
+//     printf("You set N=%d, P=%d\n",N,P);
     
-    int ans[100];
-    while(!SetData(ans,"ans:",P)||!Check(ans,N,P))
-        printf("The answer is  illegal,  please  reset  the  answer.\n");
-    HX(ans,P);
-    return 0;
-}
+//     int ans[100];
+//     while(!SetData(ans,"ans:",P)||!Check(ans,N,P))
+//         printf("The answer is  illegal,  please  reset  the  answer.\n");
+//     HX(ans,P);
+//     return 0;
+// }
--- CS-Code-Design/hw2/test.c
+++ CS-Code-Design/hw2/test.c
@@ -1,12 +1,127 @@
+
 #include<stdio.h>
 #include<stdlib.h>
-
-int main(int argc, char const *argv[])
+ 
+int main(int argc, char*argv[])
 {
-    char test[200];
-    int i = 0;
-    i = scanf("ans:%[^\n]",test);
-    printf("%s\n",test);
-    printf("%d\n",i);
-    return 0;
+       if(argc<3)
+       {
+               printf("Please input N and P from command argument list.\n");
+               return 2;
+       }
+ 
+       if(argc>3)
+       {
+               printf("%d arguments ignored.\n", argc-3);
+       }
+ 
+       int N, P;
+ 
+       N = atoi(argv[1]);
+       P = atoi(argv[2]);
+ 
+       if( N < 1 )
+       {
+               printf("Given N is illegal.\n");
+               return 2;
+       }
+ 
+       if( P < 1 || P >= 100 )
+       {
+               printf("Given P is illegal.\n");
+               return 3;
+       }
+ 
+       printf("You set N = %d, P = %d.\n", N, P);
+ 
+       int answer[P];
+       int guess[P];
+       int i;
+ 
+       start:
+       printf("ans: ");
+ 
+       for( i = 0 ; i < P ; i++)
+       {
+               scanf("%d", &answer[i]);
+               if( answer[i] > N || answer[i] <= 0 )
+               {
+                       printf("The answer is illegal, please reset the answer.\n");
+                       goto start;
+               }
+       }
+ 
+       start2:
+       printf("guess: ");
+ 
+       for( i = 0 ; i < P ; i++)
+       {
+               scanf("%d", &guess[i]);
+               if( guess[i] > N || guess[i] <= 0 )
+                       {
+                               printf("The guess is illegal, please reset your guess.\n");
+                               goto start2;
+                       }
+       }
+ 
+       int H, X;
+       int answer_count[N];
+       int guess_count[N];
+       int this_number_is, this_number_is2;
+ 
+       H = 0;
+ 
+       for( i = 0 ; i < P ; i++)
+       {
+               if( answer[i] == guess[i])
+               {
+                       H++;
+               }
+       }
+ 
+        //reset
+       for( i = 0 ; i < N ; i++ )
+       {
+               answer_count[i] = 0;
+       }
+       
+       for( i = 0 ; i < P ; ++i )
+       {
+               int this_number_is = answer[i];
+               answer_count[ this_number_is - 1] +=1;
+       }
+ 
+       for( i = 0 ; i < N ; ++i )
+       {
+               guess_count[i] = 0;
+       }
+ 
+       for( i = 0 ; i < P ; ++i )
+       {
+               int this_number_is2 = guess[i];// 假設guess[i] 是10000 那guess_count[10000-1]會造成記憶體錯誤，上面也是一樣
+               guess_count[ this_number_is2-1 ] += 1;
+       }
+ 
+       int T;
+ 
+       if(answer_count[i] > guess_count[i])//i 是 P
+       {
+               guess_count[i] = T;// T未初始化 會造成錯誤
+       }else{
+               answer_count[i] = T;
+       }
+ 
+       X = T - H ;
+ 
+       if (H==P)
+       {
+               printf("Bingo!!\n");
+       }else{
+               printf("%dH %dX\n", H, X);
+               printf("Please answer again.\n");
+               goto start2;
+       }
+ 
+       printf("The game is over.\n");
+       return 0;
 }
