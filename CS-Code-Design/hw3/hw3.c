7366a1c0c3b0dc02e08141a7f8863da108715857
Yu-Chun1214 <yuchunlin0075@gmail.com>
2018-10-19 15:20:11 +0800

    finish all requests function

--- CS-Code-Design/hw3/hw3.c
+++ CS-Code-Design/hw3/hw3.c
@@ -13,32 +13,16 @@ double ComputeExponent(double n,int exp){
         for(i = 0; i <= -exp;i++)
             num/=n;
     }
-    // printf("%lf\n",num);
     return num;
 }
 
-void OutputBitPattern_x32(int *x,int bits){
-    // printf("%d\n",*x);
-    // double test = *x;
-    // printf("%lf\n",test);
-    int j = 0;
+void OutputBitPattern(long long *x,int bits){
+    long long j = 0;
     for(int i = bits-1;i >= 0; i--){
         j = 1;
         printf("%d",*x&j<<i?1:0);
     }
     printf("\n");
-}
-
-void OutputBitpattern_x64(long long *x){
-    printf("x = %lld\n",*x);
-    long long j;
-    for(int i = 64-1;i >= 0; i--){
-        j = 1;
-        printf("%d",*x&j<<i?1:0);
-    }
-    // printf("%d",j<<63?1:0);
-    printf("\n");
-
 }
 
 int inputBitpattern_int_x32(const char *pattern,int bits){
@@ -79,15 +63,14 @@ void inputBitpattern_float(const char *pattern,int bits){
             exponent += 1<<(exponentbit - i);
         }
     }
+    printf("exponent = %d\n",exponent);
     for(i = exponentbit + 1; i < bits; i++){
         if(pattern[i] == '1'){
             mentisa += ComputeExponent(2,exponentbit - i);
         }
     }
-    printf("%Lf\n",(double)s*ComputeExponent(2,(exponent - 1023))*(1+mentisa));
+    printf("%Lf\n",(double)s*ComputeExponent(2,(exponent - (bits*28-769)))*(1+mentisa));
 }
-
-
 
 int main(int argc, char *argv[])
 {
@@ -97,36 +80,37 @@ int main(int argc, char *argv[])
         printf("count is not enough\n");
         // printf("%lf\n",ComputeExponent(2,atoi(argv[1])));
         long long x = 1;
-        OutputBitpattern_x64(&x);
+        OutputBitPattern(&x,64);
         return 0;
     }
     switch(atoi(argv[1])){
         int x;
         float y;
         double z;
+        long long w;
         case 1 :
             inputBitpattern_int_x32(argv[2],32);
             inputBitpattern_float(argv[2],32);
+            break;
         case 2 : 
             x = atoi(argv[2]); 
-            OutputBitPattern_x32(&x,32);
+            OutputBitPattern(&x,32);
+            break;
         case 3:
             y = atof(argv[2]);
-            OutputBitPattern_x32(&y,32);
+            OutputBitPattern(&y,32);
             break;
         case 4:
             inputBitpattern_int_x64(argv[2],64);
             inputBitpattern_float(argv[2],64);
             break;
-        // case 5 : 
-        //     x = atoi(argv[2]);
-        //     OutputBitpattern_x64(&x,64);
-        //     break;
-        // case 6 : 
-        //     z = atof(argv[2]);
-        //     OutputBitpattern_x64(&z,64);
-        //     break;
+        case 5 : 
+            w = atoi(argv[2]);
+            OutputBitPattern(&w,64);
+            break;
+        case 6 : 
+            z = atof(argv[2]);
+            OutputBitPattern(&z,64);
+            break;
     }
 }
-
-
--- CS-Code-Design/hw3/test.py
+++ CS-Code-Design/hw3/test.py
@@ -1,1 +1,72 @@
-print('%x' % 10)
+"""
+因為作業2有bug自己沒發現
+氣一個怒寫測試程式
+希望能幫我找bug
+
+這份測試程式會吃掉你很多網路流量
+請謹慎使用
+
+"""
+
+
+__author__ = 'Yu-Chun,Lin'
+
+import json
+import threading
+import requests
+import bs4
+from requests_html import HTMLSession
+
+
+# Test binary to Decimal
+# Test Part 1 and Part 2
+Binary_To_Decimal_x32 = 'https://www.binaryhexconverter.com/hesapla.php?fonksiyon=bin2dec&deger={}&pad=false&v=2'
+
+# Test binary to float and float to Decimal 
+Decimal_To_float_x32 = 'https://www.h-schmidt.net/FloatConverter/binary-json.py?decimal={}'
+
+Decimal_To_double_x64 = 'https://www.exploringbinary.com/floating-point-converter'
+
+session = HTMLSession()
+
+def TestFloat(num:int)->list:
+    response = session.get(Decimal_To_float_x32.format(120))
+    if response.status_code != 200:
+        return None
+    ans = json.loads(response.text)
+    return [int(ans["highprecision_decimal"]),ans["binaryRepr"]]
+
+def BitpatternTest_x32(pattern:str)->list:
+    response = session.get(Binary_To_Decimal_x32.format(pattern))
+    if response.status_code != 200:
+        return None
+    ans = response.text
+    return [int(ans),pattern]
+
+# def encoding(num):
+#     code = ''
+#     num = str(num)
+#     for i in num:
+#         if i == '-':
+#             code += '045'
+#         elif i == '.':
+#             code += '046' 
+#         elif int(i) in range(0,10):
+#             code += '0'+str(int(i)+48)        
+#     return code
+
+def TestDouble(num)->list:
+    form_data = {
+        'd2f_input_decimal': str(num),
+        'd2f_convert': 'Convert',
+        'd2f_precision_53_chosen': '1',
+        'd2f_input_decimal_format_chosen': '1',
+        'd2f_input_rawBinary_format_chosen': '1',
+    }
+    input_response = requests.post(Decimal_To_double_x64,data=form_data)
+    # print(input_response.status_code)
+    soup = bs4.BeautifulSoup(input_response.text,'html.parser')
+    target = soup.find_all('input','output_short_box')
+    target = target[0].attrs['value'].replace(' ','')
+    return [num, target]
+
