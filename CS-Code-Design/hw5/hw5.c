1a78054cc0ee3914a8cf2e58a0d2d5cd3b5993db
Yu-Chun1214 <yuchunlin0075@gmail.com>
2018-11-06 15:01:34 +0800

    finish hw5 v1

--- CS-Code-Design/hw5/hw5.c
+++ CS-Code-Design/hw5/hw5.c
@@ -14,46 +14,55 @@ void Zeroing(int *array,int size){
     for(int i = 0; i < size; i++) array[i] = 0;
 }
 
-int Merge(int * original_bucket, int * newMapping, int * bucket_index,int Max,int row){
+int Merge(int * original_bucket, int * newMapping, char * bucket_index,int Max){
     int map_match[newMapping[0] + 1];
     int no_match[original_bucket[0]+1];
+    
+    // printf("In Merge\n");
     Zeroing(no_match,original_bucket[0] + 1);
     Zeroing(map_match,newMapping[0] + 1);
+    // char * temp = (char *)malloc((Max+1) * sizeof(char));
+
+    for(int i = 0; i < Max + 1; i++) bucket_index[i] = '0';
+    bucket_index[Max+1] = '\0'; // set the end of bucket_index
+
+    // printf("%s\n",bucket_index);
+    // exit(0);
     // int successful_match;
     int result = 0,count = 0;
     int i,j;
+   
+    // system("pause");
     for(i = 1; i <= original_bucket[0]; i++){
         // successful_match = 0;
         for(j = 1; j <= newMapping[0]; j++){
             if(!map_match[j] && newMapping[j] == original_bucket[i]){
                 map_match[j] = 1;
-                printf("%d ",Max-j);
+                // printf("%d ",Max-j);
                 // successful_match = 1;
+                bucket_index[j] = '1';
                 result += 1<<(Max - j);
                 break;
             }
         }
         if(j > newMapping[0]){
             no_match[count] = original_bucket[i];
-            ++count;            
+            ++count;          
         }
-    }
+   }
     
-    if(Max - newMapping[0] < count) return 0;
+    if(Max - newMapping[0] < count){
+        // printf("failed to merge\n");
+        return 0;
+    }
     for(i = 0; i < count; i++){
         newMapping[newMapping[0] + 1] = no_match[i];
         // printf("%d ",(Max - newMapping[0] - 1));
-        result += 1 << (Max - newMapping[0] - 1);
+        // result += 1 << (Max - newMapping[0] - 1);
+        bucket_index[newMapping[0] + 1] = '1';
         newMapping[0]++;
     }
-    // printf("\n");
-    // for(int i = 0; i <= newMapping[0];i++){
-    //     printf("%d ",newMapping[i]);
-    // }
-    // printf("\n");
-    // printf("\nresult = %d\n",result);
-    // printf("count = %d\n",count);
-    
+
     return 1;
 }
 
@@ -63,62 +72,110 @@ int ** Compress(int ** original_bucket, int ** newMapping, int n,int Max){
     int successful = 0;
     int count = 0;
     int temp = 0;
-    
+    int i,j;
     char *** bucket_index = (char ***)malloc(n*sizeof(char **));
+
     for(int i = 0; i < n; i++){
         bucket_index[i] = (char **)malloc(2*sizeof(char *));
         bucket_index[i][0] = (char *)malloc(5*sizeof(char));
-        bucket_index[i][1] = (char *)malloc(Max*sizeof(char));
+        bucket_index[i][1] = (char *)malloc((Max + 2)*sizeof(char));
+        // sprintf(bucket_index[i][0],"%d",i);
     }
-
-    do{
-        // searching the appropriate row in newmapping array
-        for(int i = 0; i < newMappingRows; i++){ // every row in newMapping;
-            if(newMapping[i][0] < Max){ // if row i in newMapping isn't full
-                for(int j = 1; j < original_bucket[n][0]; j++){ // elements in original bucket
-                    for(int m = 1; j <= newMapping[i][0]; m++){ // elements in newmapping array
-                        if(original_bucket[n][j] == 0)
-                            return bucket_index;
-                    }
-                }
+    
+    
+    for(i = 0; i < n; i++){
+        for(j = 0; j < newMappingRows; j++){
+            if(Merge(original_bucket[i],newMapping[j],bucket_index[i][1],Max)){
+                sprintf(bucket_index[i][0],"%d",j);
+                // printf("successful\n");
+                break;
             }
         }
-        --n;
-        successful = 0;
-    }while(n >= 0);
+        // system("pause");
+        if(j >= newMappingRows){
+            newMappingRows ++;
+            newMapping = realloc(newMapping,(newMappingRows)*sizeof(int **));
+            newMapping[j] = (int *)malloc((Max + 1) * sizeof(int));
+            newMapping[j][0] = 0;
+            // printf("j = %d\n",j);
+            // system("pause");
+            if(Merge(original_bucket[i],newMapping[j],bucket_index[i][1],Max))
+                sprintf(bucket_index[i][0],"%d",j);
+        }
+        // printf("%s",bucket_index[i][1]);
+        // printf("-------------\n");
+        // printf("%s | %s\n",bucket_index[i][0],bucket_index[i][1]);
+        // printf("=============\n");
+        // printf("%d | ",newMapping[j][0]);
+        // for(int i = 1;  i <= newMapping[j][0]; i++) printf("%d ",newMapping[j][i]);
+    }
+    for(int i = 0; i < n; i++){
+        printf("%s %s\n",bucket_index[i][0],bucket_index[i][1]);
+    }
+    for(int i = 0; i < newMappingRows; i++){
+        printf("%d | ",newMapping[i][0]);
+        for(int j = 1;j <= newMapping[i][0]; j++){
+            printf("%5d ",newMapping[i][j]);
+        }
+        printf("\n");
+    }
+    return newMapping;
 }
 
 int main(int argc, char const *argv[])
 {
     int n = atoi(argv[1]),m = atoi(argv[2]),d = atoi(argv[3]), s = atoi(argv[4]);
-    // n is the amount of buckets
-    // the element in bucket need to small than m
-    // 0 ~ d-1 is the range of number of element
-    // s is seed of srand
     srand(s);
     int amounts_of_elements = (rand() % m) + 1;
     int **bucket = (int **)malloc(n*sizeof(int *));
     int ** newMapping;
     int bucketAmountOfNewMapping;
-    for(int i = 0; i < n; i++){
-        amounts_of_elements = (rand() % m) + 1;
-        // printf("amounts of elements = %d\n",amounts_of_elements);
-        bucket[i] = (int *)malloc((amounts_of_elements + 1)*sizeof(int)); // malloc amount_of_element + 1 '1' is for first element
-        bucket[i][0] = amounts_of_elements;
-        for(int j = 1; j <= bucket[i][0]; j++){
-            bucket[i][j] = rand()%d;
-        }
+    // for(int i = 0; i < n; i++){
+    //     amounts_of_elements = (rand() % m) + 1;
+    //     // printf("amounts of elements = %d\n",amounts_of_elements);
+    //     bucket[i] = (int *)malloc((amounts_of_elements + 1)*sizeof(int)); // malloc amount_of_element + 1 '1' is for first element
+    //     bucket[i][0] = amounts_of_elements;
+    //     for(int j = 1; j <= bucket[i][0]; j++){
+    //         bucket[i][j] = rand()%d;
+    //     }
+    // }
+    {
+        bucket[0] = (int *)malloc(5 * sizeof(int));
+        bucket[0][0] = 4;bucket[0][1] = 1;bucket[0][2] = 2;bucket[0][3] = 5;bucket[0][4] = 7;
+
+        bucket[1] = (int *)malloc(5 * sizeof(int));
+        bucket[1][0] = 4;bucket[1][1] = 2;bucket[1][2] = 3;bucket[1][3] = 5;bucket[1][4] = 7;
+
+        bucket[2] = (int *)malloc(4 * sizeof(int));
+        bucket[2][0] = 3;bucket[2][1] = 2;bucket[2][2] = 4;bucket[2][3] = 8;
+
+        bucket[3] = (int *)malloc(4 * sizeof(int));
+        bucket[3][0] = 3;bucket[3][1] = 2;bucket[3][2] = 3;bucket[3][3] = 8;
+
+        bucket[4] = (int *)malloc(3 * sizeof(int));
+        bucket[4][0] = 2; bucket[4][1] = 2; bucket[4][2] = 7;
+
+        bucket[5] = (int *)malloc(3 * sizeof(int));
+        bucket[5][0] = 2; bucket[5][1] = 1; bucket[5][2] = 3; 
+
+        bucket[6] = (int *)malloc(2 * sizeof(int));
+        bucket[6][0] = 1; bucket[6][1] = 2;
+
+        bucket[7] = (int *)malloc(4 *sizeof(int));
+        bucket[7][0] = 3; bucket[7][1] = 1; bucket[7][2] = 5;bucket[7][3] = 6;
+
+        bucket[8] = (int *)malloc(4 *sizeof(int));
+        bucket[8][0] = 3; bucket[8][1] = 1; bucket[8][2] = 5; bucket[8][3] = 7;
     }
     for(int i = 0; i < n; i++){
         printf("%d | ",bucket[i][0]);
         for(int j = 1;j <= bucket[i][0];j++){
-            printf("%d ",bucket[i][j]);
+            printf("%5d",bucket[i][j]);
         }
         printf("\n");
     }
     bucketAmountOfNewMapping = MaxAmountOfElementInBucket(bucket,n);
-    int test1[5] = {3, 1, 5, 4, 4};
-    int test2[7] = {4, 2, 4, 3, 2, 4};
-    Merge(test1,test2,test1,7,3);
+    printf("Max = %d\n",bucketAmountOfNewMapping);
+    Compress(bucket,newMapping,n,bucketAmountOfNewMapping);
     return 0;
 }
