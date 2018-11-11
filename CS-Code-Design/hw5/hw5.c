294fff8854f348f8188f18641b17e1f08a2839a9
Yu-Chun1214 <yuchunlin0075@gmail.com>
2018-11-11 20:26:48 +0800

    commit

--- CS-Code-Design/hw5/hw5.c
+++ CS-Code-Design/hw5/hw5.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#define EOB -1
 
 int MaxAmountOfElementInBucket(int **original_bucket, int n){
     int Max = 0;
@@ -41,7 +42,7 @@ int Merge(int * original_bucket, int * newMapping, char * bucket_index,int Max,i
         }
    }
     
-    if(Max - *newMapping0 < count) return EOF;
+    if(Max - *newMapping0 < count) return EOB;
     for(i = 0; i < count; i++){
         newMapping[*newMapping0 + 1] = no_match[i];
         bucket_index[*newMapping0 + 1] = '1';
@@ -64,7 +65,7 @@ int bucket_union(int newMappingRows,int *original_bucket,int ** newMapping,char 
     printf("max_match = %d\n",max_match);
     printf("max_match_row = %d\n",max_match_row);
     printf("============================\n");
-    return max_match == EOF ? max_match : max_match_row;
+    return max_match == EOB ? max_match : max_match_row;
 }
 
 int ** Compress(int ** original_bucket, int ** newMapping, int n,int Max){
@@ -84,25 +85,11 @@ int ** Compress(int ** original_bucket, int ** newMapping, int n,int Max){
     }
     
     for(i = 0; i < n; i++){
-        max_match = EOF;
-        // for(int k = 1; k <= original_bucket[i][0]; k++)
-        //     printf("%5d",original_bucket[i][k]);
-        // printf("\n");
-        // for(j = 0; j < newMappingRows; j++){
-        //     or_mapping = newMapping[j][0];
-        //     temp = Merge(original_bucket[i],newMapping[j],bucket_index[i][1],Max,&or_mapping);
-        //     max_match = (temp > max_match) ? max_match_row = j,temp : max_match; 
-        // }
+        max_match = EOB;
         max_match = bucket_union(newMappingRows,original_bucket[i],newMapping,bucket_index[i],Max);
-        // printf("max_match_row = %d\n",max_match_row);
-        // printf("max_match = %d\n",max_match);
-        // system("pause");
-        if(max_match == EOF){
-            
-            // newMapping = realloc(newMapping,(newMappingRows  + 1)*sizeof(int **));
+        if(max_match == EOB){
             newMapping[newMappingRows] = (int *)malloc((Max + 1) * sizeof(int));
             newMapping[newMappingRows][0] = 0;
-            // printf("newMappingRows = %d\n",newMappingRows);
             Merge(original_bucket[i],newMapping[newMappingRows],bucket_index[i][1],Max,&newMapping[newMappingRows][0]);
             sprintf(bucket_index[i][0],"%d",newMappingRows);
             newMappingRows ++;
@@ -111,13 +98,6 @@ int ** Compress(int ** original_bucket, int ** newMapping, int n,int Max){
             Merge(original_bucket[i],newMapping[max_match_row],bucket_index[i][1],Max,&newMapping[max_match_row][0]);
             sprintf(bucket_index[i][0],"%d",max_match_row);
         }
-        // for(int k = 0; k < newMappingRows; k++){
-        //     printf("%d | ",newMapping[k][0]);
-        //     for(int l = 1; l <= newMapping[k][0]; l++)
-        //         printf("%5d",newMapping[k][l]);
-        //     printf("\n");
-        // }
-        // system("pause");
     }
     for(int i = 0; i < n; i++){
         printf("%s %s\n",bucket_index[i][0],bucket_index[i][1]);
@@ -140,43 +120,14 @@ int main(int argc, char const *argv[])
     int **bucket = (int **)malloc(n*sizeof(int *));
     int ** newMapping;
     int bucketAmountOfNewMapping;
-    // for(int i = 0; i < n; i++){
-    //     amounts_of_elements = (rand() % m) + 1;
-    //     // printf("amounts of elements = %d\n",amounts_of_elements);
-    //     bucket[i] = (int *)malloc((amounts_of_elements + 1)*sizeof(int)); // malloc amount_of_element + 1 '1' is for first element
-    //     bucket[i][0] = amounts_of_elements;
-    //     for(int j = 1; j <= bucket[i][0]; j++){
-    //         bucket[i][j] = rand()%d;
-    //     }
-    // }
-
-    {
-        bucket[0] = (int *)malloc(5 * sizeof(int));
-        bucket[0][0] = 4;bucket[0][1] = 1;bucket[0][2] = 2;bucket[0][3] = 5;bucket[0][4] = 7;
-
-        bucket[1] = (int *)malloc(5 * sizeof(int));
-        bucket[1][0] = 4;bucket[1][1] = 2;bucket[1][2] = 3;bucket[1][3] = 5;bucket[1][4] = 7;
-
-        bucket[2] = (int *)malloc(4 * sizeof(int));
-        bucket[2][0] = 3;bucket[2][1] = 2;bucket[2][2] = 4;bucket[2][3] = 8;
-
-        bucket[3] = (int *)malloc(6 * sizeof(int));
-        bucket[3][0] = 3;bucket[3][1] = 2;bucket[3][2] = 3;bucket[3][3] = 8; bucket[3][4] = 4; bucket[3][5] = 5;
-
-        bucket[4] = (int *)malloc(3 * sizeof(int));
-        bucket[4][0] = 2; bucket[4][1] = 2; bucket[4][2] = 7;
-
-        bucket[5] = (int *)malloc(3 * sizeof(int));
-        bucket[5][0] = 2; bucket[5][1] = 1; bucket[5][2] = 3; 
-
-        bucket[6] = (int *)malloc(2 * sizeof(int));
-        bucket[6][0] = 1; bucket[6][1] = 2;
-
-        bucket[7] = (int *)malloc(4 *sizeof(int));
-        bucket[7][0] = 3; bucket[7][1] = 1; bucket[7][2] = 5;bucket[7][3] = 6;
-
-        bucket[8] = (int *)malloc(4 *sizeof(int));
-        bucket[8][0] = 3; bucket[8][1] = 1; bucket[8][2] = 5; bucket[8][3] = 7;
+    for(int i = 0; i < n; i++){
+        amounts_of_elements = (rand() % m) + 1;
+        // printf("amounts of elements = %d\n",amounts_of_elements);
+        bucket[i] = (int *)malloc((amounts_of_elements + 1)*sizeof(int)); // malloc amount_of_element + 1 '1' is for first element
+        bucket[i][0] = amounts_of_elements;
+        for(int j = 1; j <= bucket[i][0]; j++){
+            bucket[i][j] = rand()%d;
+        }
     }
     for(int i = 0; i < n; i++){
         printf("%d | ",bucket[i][0]);
