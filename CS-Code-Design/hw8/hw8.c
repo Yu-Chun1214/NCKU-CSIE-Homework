29a2ff664b77b0b943eebd2fb5247eef2c77b8b9
Yu-Chun1214 <yuchunlin0075@gmail.com>
2018-12-24 17:50:46 +0800

    checkout to edit the basic operation bug

--- CS-Code-Design/hw8/hw8.c
+++ CS-Code-Design/hw8/hw8.c
@@ -8,13 +8,14 @@ struct Node{
     nodePointer link;
 };
 
+typedef nodePointer * listPointer;
 typedef struct Node Node;
 
 nodePointer Constructor(int i);
 void Destructor(nodePointer trash);
 void Insertion(nodePointer node1, nodePointer node2); // node1->link whill be inserted after node2
-nodePointer createRandomChain(int amounts);
-void showLinkedList(nodePointer  header);
+listPointer createRandomChain(int amounts);
+void showLinkedList(listPointer);
 
 
 nodePointer Constructor(int i){
@@ -43,19 +44,19 @@ void Insertion(nodePointer node1, nodePointer node2){
     node2->link->link = temp;
 }
 
-nodePointer createRandomChain(int amounts){
-    Node headerNode;
-    nodePointer currentNode;
-    currentNode = &headerNode;
-    for(int i = 0; i < amounts; i++){
-        currentNode->link = Constructor(i);
-        currentNode = currentNode->link;
+listPointer createRandomChain(int amounts){
+    listPointer start;
+    nodePointer current = Constructor(0);
+    *start = current;
+    for(int i = 1; i <amounts; i++){
+        current->link = Constructor(i);
+        current = current->link;
     }
-    return headerNode.link;
+    return start;
 }
 
-void showLinkedList(nodePointer  header){
-    nodePointer current = header;
+void showLinkedList(listPointer header){
+    nodePointer current = *header;
     while(current){
         printf("%d ",current->value);
         current = current->link;
@@ -63,21 +64,45 @@ void showLinkedList(nodePointer  header){
     printf("\n");
 }
 
+void * pivot_selection(void * v_chain,unsigned int size){
+    nodePointer chain = (nodePointer)v_chain;
+    nodePointer iterator = chain;
+    for(int i = 0; i < size-1;i++) iterator = iterator->link;
+    return (void *) iterator;
+}
+
+void quickSort(void * chain,unsigned int size, int cmpr(void *,void *),void insertion(void *, void *),void * pivot_selection(void * chain,unsigned int size)){
+    void * pivot = pivot_selection(chain,size - 1); // select which node you want to be the pivot
+    // void * before_pivot = pivot_selection(chain,size - 1);
+    void * iterator;
+    int i = 0; void * first_swap,* current = chain;
+    for(i = 0; i < size; i++){
+        if(cmpr(current,pivot)){
+            first_swap = current;
+            insertion(current,NULL);
+        }
+    }
+}
+
+int cmpr(void * v_node1, void * v_node2){
+    nodePointer node1 = (nodePointer)v_node1;
+    nodePointer node2 = (nodePointer)v_node2;
+    if(node1->value > node2->link->value) return 1;
+    return 0;
+}
+
 int main(int argc, char const *argv[])
 {
     srand(time(NULL));
-    nodePointer chain;
+    // nodePointer chain;
     nodePointer current;
-    current = chain;
-    chain = createRandomChain(5);
+    listPointer chain;
     printf("showLinkedList\n");
     showLinkedList(chain);
     current = chain;
-    for(int i = 0; i < 3; i++){
-        current = current->link;
-    }
+    current = (nodePointer)pivot_selection((void *)chain,3);
     printf("current->value = %d\n",current->value);
-    Insertion(current,chain);
+    Insertion(chain,current);
     showLinkedList(chain);
     return 0;
 }
