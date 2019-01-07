278afb1927f18552ee482070cf7f4f88ac402ab4
Yu-Chun1214 <yuchunlin0075@gmail.com>
2019-01-07 14:15:53 +0800

    finish no.6

--- CS-Code-Design/practice/test.c
+++ CS-Code-Design/practice/test.c
@@ -1,53 +1,54 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-#include <math.h>
-
-typedef union{
-    int integer;
-    double floating;
-}testunion1;
-
-typedef union{
-    int integer1;
-    int integer2;
-}testunion2;
 
 typedef struct{
-    int integer1 : 4;
-    int integer2 : 4;
-}bitfield;
+    short int ip[16];
+    short int length;
+}ipv6;
 
-// 以long long pointer 來存 位置 是因為這樣32bits 可以用 64bits也可以用
-void OutputBitPattern(long long *x,int bits){
-    printf("%d-bit: ",bits);
+char * OutputBitPattern(long long *x,int bits){
+    // printf("%d-bit: ",bits);
     long long j = 0;
     int i;
+    int ch_it = 0;
+    char * bp = (char *)malloc((bits + 1) * sizeof(char));
     for(i = bits-1;i >= 0; i--){
         j = 1;
-        printf("%d",*x&j<<i?1:0);
+        // printf("%d",*x&j<<i?1:0);
+        bp[ch_it] = *x&j<<i?'1':'0';
+        ch_it++;
     }
-    printf("\n");
+    // printf("%s\n",bp);
+    bp[bits + 1] = '\0';
+    // printf("\n");
+    return bp;
 }
 
-
-int main(){
-    testunion1 u;
-    u.floating = 10;
-    u.integer = 6666666666666666666;
-    printf("%p\n",&u.floating);
-    printf("%p\n",&u.integer);
-    printf("%.64lf\n",u.floating);
-    printf("%d\n",u.integer);
-    OutputBitPattern(&u.integer,8 * sizeof(u.integer));
-    OutputBitPattern(&u.floating,64);
-
-    testunion2 u2;
-    u2.integer1 = 5;
-    u2.integer2 = 10;
-    printf("%p\n",&u2.integer1);
-    printf("%p\n",&u2.integer2);
-    printf("%d\n",u2.integer1);
-    printf("%d\n",u2.integer2);
-
-    
+int main(int argc, char const *argv[])
+{
+    FILE * file = fopen("file.txt","r");
+    short int a[5];
+    ipv6 ips[10];
+    int i = 0;
+    while(EOF != fscanf(file,"%2x%2x:%2x%2x:%2x%2x:%2x%2x:%2x%2x:%2x%2x:%2x%2x:%2x%2x/%2d",&ips[i].ip[0],&ips[i].ip[1],&ips[i].ip[2],&ips[i].ip[3],&ips[i].ip[4],&ips[i].ip[5],&ips[i].ip[6],&ips[i].ip[7],&ips[i].ip[8],&ips[i].ip[9],&ips[i].ip[10],&ips[i].ip[11],&ips[i].ip[12],&ips[i].ip[13],&ips[i].ip[14],&ips[i].ip[15],&ips[i].length)){
+        // fscanf(file,"%2x%2x:%2x%2x:%2x%2x:%2x%2x:%2x%2x:%2x%2x:%2x%2x:%2x%2x/%2d",&ips[i].ip[0],&ips[i].ip[1],&ips[i].ip[2],&ips[i].ip[3],&ips[i].ip[4],&ips[i].ip[5],&ips[i].ip[6],&ips[i].ip[7],&ips[i].ip[8],&ips[i].ip[9],&ips[i].ip[10],&ips[i].ip[11],&ips[i].ip[12],&ips[i].ip[13],&ips[i].ip[14],&ips[i].ip[15],&ips[i].length);
+        printf("%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x/%02d\n",ips[i].ip[0],ips[i].ip[1],ips[i].ip[2],ips[i].ip[3],ips[i].ip[4],ips[i].ip[5],ips[i].ip[6],ips[i].ip[7],ips[i].ip[8],ips[i].ip[9],ips[i].ip[10],ips[i].ip[11],ips[i].ip[12],ips[i].ip[13],ips[i].ip[14],ips[i].ip[15],ips[i].length);
+        i++;
+        
+    }
+    int count = i;
+    // printf("%s\n",OutputBitPattern(&ips[0].ip[0],8));
+    for(i = 0; i < count; i++){
+        printf("%s%s:",OutputBitPattern(&(ips[i].ip[0]),8),OutputBitPattern(&(ips[i].ip[1]),8));
+        for(int j = 2; j < 16; j+=2){
+            printf(":%s%s",OutputBitPattern(&(ips[i].ip[j]),8),OutputBitPattern(&(ips[i].ip[j+1]),8));
+        }
+        printf("/");
+        for(int j = 0; j < ips[i].length;j++)
+            printf("1");
+        for(int j = 0; j < 128 - ips[i].length; j++)
+            printf("0");
+        printf("\n");
+    }
 }
