732be8cd6a19324e692b8c8a17f0bcab3396f02a
Yu-Chun1214 <yuchunlin0075@gmail.com>
2019-01-07 23:17:49 +0800

    update fibo matrix

--- CS-Code-Design/practice/test.c
+++ CS-Code-Design/practice/test.c
@@ -2,27 +2,25 @@
 #include <stdlib.h>
 
 void fibo_row(int **a,int col,int num){
-    a[col][num] = a[col][num + 1] = 1;
     for(int i = num + 2; i < 10; i++){
         a[col][i] = a[col][i - 1] + a[col][i - 2];
     }
 }
 
 void fibo_col(int **a,int col,int num){
-    a[num][col] = a[num + 1][col] = 1;
     for(int i = num + 2; i < 10; i++){
         a[i][col] = a[i - 1][col] + a[i - 2][col];
     }
 }
 
 int main(){
-    int ** a = (int **)malloc(11 * sizeof(int *));
+    int ** a = (int **)malloc(10 * sizeof(int *));
     for(int i = 0; i < 11; i++)
-        a[i] = (int *)malloc(11 * sizeof(int));
+        a[i] = (int *)malloc(10 * sizeof(int));
 
     for(int i = 0; i < 10; i++)
         for(int j = 0; j < 10; j++)
-            a[i][j] = 0;
+            a[i][j] = 1;
     
     for(int i = 0; i < 10; i++){
         fibo_row(a,i,i);
