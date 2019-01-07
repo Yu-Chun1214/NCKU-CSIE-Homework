79aa17709e1965cea45a50d0d5d4e926917a8c37
Yu-Chun1214 <yuchunlin0075@gmail.com>
2019-01-07 23:09:26 +0800

    finish fibo matrix

--- CS-Code-Design/practice/test.c
+++ CS-Code-Design/practice/test.c
@@ -1,64 +1,37 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 
-typedef struct{
-    short int ip[16];
-    short int length;
-}ipv6;
-
-char * OutputBitPattern(long long *x,int bits){
-    // printf("%d-bit: ",bits);
-    long long j = 0;
-    int i;
-    int ch_it = 0;
-    char * bp = (char *)malloc((bits + 1) * sizeof(char));
-    for(i = bits-1;i >= 0; i--){
-        j = 1;
-        // printf("%d",*x&j<<i?1:0);
-        bp[ch_it] = *x&j<<i?'1':'0';
-        ch_it++;
+void fibo_row(int **a,int col,int num){
+    a[col][num] = a[col][num + 1] = 1;
+    for(int i = num + 2; i < 10; i++){
+        a[col][i] = a[col][i - 1] + a[col][i - 2];
     }
-    // printf("%s\n",bp);
-    bp[bits + 1] = '\0';
-    // printf("\n");
-    return bp;
 }
 
-int main(int argc, char const *argv[])
-{
-    FILE * file = fopen("file.txt","r");
-    short int a[5];
-    ipv6 ips[10];
-    int i = 0;
-    int isEOF = 0;
-    // while(EOF != fscanf(file,"%2x%2x:%2x%2x:%2x%2x:%2x%2x:%2x%2x:%2x%2x:%2x%2x:%2x%2x/%2d",&ips[i].ip[0],&ips[i].ip[1],&ips[i].ip[2],&ips[i].ip[3],&ips[i].ip[4],&ips[i].ip[5],&ips[i].ip[6],&ips[i].ip[7],&ips[i].ip[8],&ips[i].ip[9],&ips[i].ip[10],&ips[i].ip[11],&ips[i].ip[12],&ips[i].ip[13],&ips[i].ip[14],&ips[i].ip[15],&ips[i].length)){
-    //     // fscanf(file,"%2x%2x:%2x%2x:%2x%2x:%2x%2x:%2x%2x:%2x%2x:%2x%2x:%2x%2x/%2d",&ips[i].ip[0],&ips[i].ip[1],&ips[i].ip[2],&ips[i].ip[3],&ips[i].ip[4],&ips[i].ip[5],&ips[i].ip[6],&ips[i].ip[7],&ips[i].ip[8],&ips[i].ip[9],&ips[i].ip[10],&ips[i].ip[11],&ips[i].ip[12],&ips[i].ip[13],&ips[i].ip[14],&ips[i].ip[15],&ips[i].length);
-    //     printf("%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x/%02d\n",ips[i].ip[0],ips[i].ip[1],ips[i].ip[2],ips[i].ip[3],ips[i].ip[4],ips[i].ip[5],ips[i].ip[6],ips[i].ip[7],ips[i].ip[8],ips[i].ip[9],ips[i].ip[10],ips[i].ip[11],ips[i].ip[12],ips[i].ip[13],ips[i].ip[14],ips[i].ip[15],ips[i].length);
-    //     i++;
-    // }
-    int j = 0;
-    while(EOF != isEOF){
-        j = 0;
-        for(int k = 0; k < 7; k++){
-            isEOF = fscanf(file,"%2x%2x:",&ips[i].ip[j],&ips[i].ip[j + 1]);
-            j+=2;
-        }
-        isEOF = fscanf(file,"%2x%2x/%2d",&ips[i].ip[14],&ips[i].ip[15],&ips[i].length);
-        i++;
+void fibo_col(int **a,int col,int num){
+    a[num][col] = a[num + 1][col] = 1;
+    for(int i = num + 2; i < 10; i++){
+        a[i][col] = a[i - 1][col] + a[i - 2][col];
     }
+}
 
-    int count = i - 1;
-    for(i = 0; i < count; i++){
-        printf("%s%s:",OutputBitPattern(&(ips[i].ip[0]),8),OutputBitPattern(&(ips[i].ip[1]),8));
-        for(int j = 2; j < 16; j+=2){
-            printf(":%s%s",OutputBitPattern(&(ips[i].ip[j]),8),OutputBitPattern(&(ips[i].ip[j+1]),8));
-        }
-        printf("/");
-        for(int j = 0; j < ips[i].length;j++)
-            printf("1");
-        for(int j = 0; j < 128 - ips[i].length; j++)
-            printf("0");
+int main(){
+    int ** a = (int **)malloc(11 * sizeof(int *));
+    for(int i = 0; i < 11; i++)
+        a[i] = (int *)malloc(11 * sizeof(int));
+
+    for(int i = 0; i < 10; i++)
+        for(int j = 0; j < 10; j++)
+            a[i][j] = 0;
+    
+    for(int i = 0; i < 10; i++){
+        fibo_row(a,i,i);
+        fibo_col(a,i,i); 
+    } 
+
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 10; j++)
+            printf("%5d",a[i][j]);
         printf("\n");
     }
 }
